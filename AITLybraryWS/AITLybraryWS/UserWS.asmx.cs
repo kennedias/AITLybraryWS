a960d1d6a2eaa436b9f5f016872062f82a27bd2a
kennedias <5399@ait.nsw.edu.au>
2016-09-28 10:12:54 +1000

    first commit to userWS

---
+++ AITLybraryWS/AITLybraryWS/UserWS.asmx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using BusinessLogic;
+using System.Data;
+using SystemFramework;
+
+namespace AITLybraryWS
+{
+    /// <summary>
+    /// Summary description for UserWS
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class UserWS : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public DataTable UserLogin(string username, string pwd)
+        {
+            UserLogic userLogic = new UserLogic();
+
+            return userLogic.PerformLogin(username, pwd).ToDataTable();
+        }
+    }
+}
---
+++ BusinessLogic/SystemFramework/AppUtil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.ComponentModel;
+
+namespace SystemFramework
+{
+    public static class AppUtil
+    {
+        public static DataTable ToDataTable<T>(this IList<T> data)
+        {
+            // Extract Property information
+            PropertyDescriptorCollection propertyCollection = TypeDescriptor.GetProperties(typeof(T));
+
+            // Create Empty DataTable
+            DataTable dataTable = new DataTable();
+            dataTable.TableName = "wsTable";
+
+
+            // Add column to empty DataTable
+            for (int i = 0; i < propertyCollection.Count; i++)
+            {
+                PropertyDescriptor propertyDescriptor = propertyCollection[i];
+                dataTable.Columns.Add(propertyDescriptor.Name, propertyDescriptor.PropertyType);
+            }
+
+            // Insert Data
+            object[] rowsList = new object[propertyCollection.Count];
+
+            foreach (T item in data)
+            {
+                for (int i = 0; i < rowsList.Length; i++)
+                {
+                    rowsList[i] = propertyCollection[i].GetValue(item);
+                }
+
+                dataTable.Rows.Add(rowsList);
+            }
+
+            return dataTable;
+        }
+    }
+}
